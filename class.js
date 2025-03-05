bc9c4e0f5d798e61f92f9f6d106cf471f8cc4fd9
24csbob50 <nk24csb0b50@student.nitw.ac.in>
2025-03-05 12:17:53 +0530

    class.js

--- class.js
+++ class.js
@@ -84,10 +84,10 @@ class Product {
             new Product(2, "Smartphone", 599.99, "https://m.media-amazon.com/images/I/61Qe0euJJZL.jpg"),
             new Product(3, "Headphones", 199.99, "https://m.media-amazon.com/images/I/61Qe0euJJZL.jpg"),
             new Product(4, "Watch", 149.99, "https://m.media-amazon.com/images/I/61Qe0euJJZL.jpg"),
-            new Product(4, "Wireless earphones", 49.99, "https://m.media-amazon.com/images/I/61Qe0euJJZL.jpg"),
-            new Product(4, "Keyboard", 300.99, "https://m.media-amazon.com/images/I/61Qe0euJJZL.jpg"),
-            new Product(4, "Xiaomi 15 Ultra", 500, "https://m.media-amazon.com/images/I/61Qe0euJJZL.jpg"),
-            new Product(4, "Smart Security Camera", 250, "https://m.media-amazon.com/images/I/61Qe0euJJZL.jpg"),
+            new Product(4, "Watch", 149.99, "https://m.media-amazon.com/images/I/61Qe0euJJZL.jpg"),
+            new Product(4, "Watch", 149.99, "https://m.media-amazon.com/images/I/61Qe0euJJZL.jpg"),
+            new Product(4, "Watch", 149.99, "https://m.media-amazon.com/images/I/61Qe0euJJZL.jpg"),
+            new Product(4, "Watch", 149.99, "https://m.media-amazon.com/images/I/61Qe0euJJZL.jpg"),
         ];
 
         const productListElement = document.getElementById("product-list");
